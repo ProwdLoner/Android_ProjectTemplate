cfe80d418cd8aa32fe432299a93b78ec415fd43f
loner <prowdloner@petlogon.com>
2022-03-03 13:03:26 +0900

    -

--- app/src/main/java/com/example/prowd_android_template/activity_set/activity_basic_bottom_sheet_navigation_sample/ActivityBasicBottomSheetNavigationSample.kt
+++ app/src/main/java/com/example/prowd_android_template/activity_set/activity_basic_bottom_sheet_navigation_sample/ActivityBasicBottomSheetNavigationSample.kt
@@ -1,12 +1,183 @@
 package com.example.prowd_android_template.activity_set.activity_basic_bottom_sheet_navigation_sample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.prowd_android_template.R
+import com.example.prowd_android_template.custom_view.DialogConfirm
+import com.example.prowd_android_template.custom_view.DialogProgressLoading
+import com.example.prowd_android_template.databinding.ActivityBasicBottomSheetNavigationSampleBinding
 
 class ActivityBasicBottomSheetNavigationSample : AppCompatActivity() {
+    // <멤버 변수 공간>
+    // (뷰 바인더 객체)
+    private lateinit var bindingMbr: ActivityBasicBottomSheetNavigationSampleBinding
+
+    // (뷰 모델 객체)
+    private lateinit var viewModelMbr: ActivityBasicBottomSheetNavigationSampleViewModel
+
+    // (다이얼로그 객체)
+    // 로딩 다이얼로그
+    private var progressLoadingDialogMbr: DialogProgressLoading? = null
+
+    // 네트워크 에러 다이얼로그(타임아웃 등 retrofit 반환 에러)
+    private var networkErrorDialogMbr: DialogConfirm? = null
+
+    // 서버 에러 다이얼로그(정해진 서버 반환 코드 외의 상황)
+    private var serverErrorDialogMbr: DialogConfirm? = null
+
+
+    // ---------------------------------------------------------------------------------------------
+    // <클래스 생명주기 공간>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_basic_bottom_sheet_navigation_sample)
+
+        // (뷰 객체 바인딩)
+        bindingMbr = ActivityBasicBottomSheetNavigationSampleBinding.inflate(layoutInflater)
+        setContentView(bindingMbr.root)
+
+        // (초기 객체 생성)
+        createMemberObjects()
+        // 뷰모델 저장 객체 생성 = 뷰모델 내에 저장되어 destroy 까지 쭉 유지되는 데이터 초기화
+        createViewModelDataObjects()
+
+        // (라이브 데이터 설정 : 뷰모델 데이터 반영 작업)
+        setLiveData()
+
+        // (초기 뷰 설정)
+        viewSetting()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // (데이터 갱신 시점 적용)
+        if (!viewModelMbr.isChangingConfigurationsMbr) { // 화면 회전이 아닐 때
+
+            val sessionToken =
+                viewModelMbr.loginPrefMbr.getString(
+                    getString(R.string.pref_login),
+                    null
+                )
+
+            if (viewModelMbr.isDataFirstLoadingMbr || // 데이터 최초 로딩 시점일 때 혹은,
+                sessionToken != viewModelMbr.currentUserSessionTokenMbr // 액티비티 유저와 세션 유저가 다를 때
+            ) {
+                // 진입 플래그 변경
+                viewModelMbr.isDataFirstLoadingMbr = false
+                viewModelMbr.currentUserSessionTokenMbr = sessionToken
+
+                //  데이터 로딩
+            }
+        }
+
+    }
+
+    override fun onStop() {
+        // 설정 변경(화면회전)을 했는지 여부를 초기화
+        viewModelMbr.isChangingConfigurationsMbr = false
+        super.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // 설정 변경(화면회전)을 했는지 여부를 반영
+        viewModelMbr.isChangingConfigurationsMbr = true
+    }
+
+    override fun onDestroy() {
+        // 다이얼로그 객체 해소
+        networkErrorDialogMbr?.dismiss()
+        serverErrorDialogMbr?.dismiss()
+        progressLoadingDialogMbr?.dismiss()
+
+        super.onDestroy()
+    }
+
+
+    // ---------------------------------------------------------------------------------------------
+    // <공개 메소드 공간>
+
+
+    // ---------------------------------------------------------------------------------------------
+    // <비공개 메소드 공간>
+    // 초기 멤버 객체 생성
+    private fun createMemberObjects() {
+        // 뷰 모델 객체 생성
+        viewModelMbr =
+            ViewModelProvider(this)[ActivityBasicBottomSheetNavigationSampleViewModel::class.java]
+
+    }
+
+    // viewModel 저장용 데이터 초기화
+    private fun createViewModelDataObjects() {
+        if (!viewModelMbr.isChangingConfigurationsMbr) { // 설정 변경(화면회전)이 아닐 때에 발동
+
+            // 로그인 데이터 객체 생성
+            viewModelMbr.loginPrefMbr = this.getSharedPreferences(
+                getString(R.string.pref_login),
+                Context.MODE_PRIVATE
+            )
+
+            // 현 액티비티 진입 유저 저장
+            viewModelMbr.currentUserSessionTokenMbr =
+                viewModelMbr.loginPrefMbr.getString(
+                    getString(R.string.pref_login_session_token_string),
+                    null
+                )
+        }
+    }
+
+    // 초기 뷰 설정
+    private fun viewSetting() {
+
+
+    }
+
+    // 라이브 데이터 설정
+    private fun setLiveData() {
+        // 로딩 다이얼로그 출력 플래그
+        viewModelMbr.progressLoadingDialogInfoLiveDataMbr.observe(this) {
+            if (it != null) {
+                progressLoadingDialogMbr = DialogProgressLoading(
+                    this,
+                    it
+                )
+                progressLoadingDialogMbr?.show()
+            } else {
+                progressLoadingDialogMbr?.dismiss()
+                progressLoadingDialogMbr = null
+            }
+        }
+
+        // 네트워크 에러 다이얼로그 출력 플래그
+        viewModelMbr.networkErrorDialogInfoLiveDataMbr.observe(this) {
+            if (it != null) {
+                networkErrorDialogMbr = DialogConfirm(
+                    this,
+                    it
+                )
+                networkErrorDialogMbr?.show()
+            } else {
+                networkErrorDialogMbr?.dismiss()
+                networkErrorDialogMbr = null
+            }
+        }
+
+        // 서버 에러 다이얼로그 출력 플래그
+        viewModelMbr.serverErrorDialogInfoLiveDataMbr.observe(this) {
+            if (it != null) {
+                serverErrorDialogMbr = DialogConfirm(
+                    this,
+                    it
+                )
+                serverErrorDialogMbr?.show()
+            } else {
+                serverErrorDialogMbr?.dismiss()
+                serverErrorDialogMbr = null
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/prowd_android_template/activity_set/activity_basic_tab_layout_sample/ActivityBasicTabLayoutSampleViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.prowd_android_template.activity_set.activity_basic_tab_layout_sample
+
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.prowd_android_template.custom_view.DialogConfirm
+import com.example.prowd_android_template.custom_view.DialogProgressLoading
+import com.example.prowd_android_template.repository.RepositorySet
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class ActivityBasicTabLayoutSampleViewModel(application: Application) :
+    AndroidViewModel(application) {
+    // <멤버 변수 공간>
+    private val applicationMbr = application
+
+    // (repository 모델)
+    private val repositorySetMbr: RepositorySet = RepositorySet.getInstance(applicationMbr)
+
+    // (스레드 풀)
+    var executorServiceMbr: ExecutorService? = Executors.newCachedThreadPool()
+
+    // (데이터)
+    // 이 화면에 도달한 유저 계정 고유값(세션 토큰이 없다면 비회원 상태)
+    var currentUserSessionTokenMbr: String? = null
+
+    // 로그인 정보 객체
+    lateinit var loginPrefMbr: SharedPreferences
+
+    // (플래그 데이터)
+    // 설정 변경 여부 : 의도적인 액티비티 종료가 아닌 화면 회전과 같은 상황
+    var isChangingConfigurationsMbr = false
+
+    // 데이터 수집 등, 첫번째에만 발동
+    var isDataFirstLoadingMbr = true
+
+
+    // ---------------------------------------------------------------------------------------------
+    // <뷰모델 라이브데이터 공간>
+    // 네트워크 에러 다이얼로그 출력 정보
+    var networkErrorDialogInfoLiveDataMbr: MutableLiveData<DialogConfirm.DialogInfoVO> =
+        MutableLiveData(null)
+
+    // 서버 에러 다이얼로그 출력 정보
+    var serverErrorDialogInfoLiveDataMbr: MutableLiveData<DialogConfirm.DialogInfoVO> =
+        MutableLiveData(null)
+
+    // 로딩 다이얼로그 출력 정보
+    var progressLoadingDialogInfoLiveDataMbr: MutableLiveData<DialogProgressLoading.DialogInfoVO> =
+        MutableLiveData(null)
+
+
+    // ---------------------------------------------------------------------------------------------
+    // <클래스 생명주기 공간>
+    override fun onCleared() {
+        executorServiceMbr?.shutdown()
+        executorServiceMbr = null
+        super.onCleared()
+    }
+
+
+    // ---------------------------------------------------------------------------------------------
+    // <공개 메소드 공간>
+
+
+    // ---------------------------------------------------------------------------------------------
+    // <비공개 메소드 공간>
+}
