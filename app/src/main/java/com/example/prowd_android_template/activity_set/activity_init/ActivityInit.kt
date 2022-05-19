bce7c9e94425cf63338b53c2f320c88505f3ef32
loner <prowdloner@petlogon.com>
2022-05-19 12:25:48 +0900

    init activity 권한 요청을 manifest 설정에 맞추기

--- app/src/main/java/com/example/prowd_android_template/activity_set/activity_init/ActivityInit.kt
+++ app/src/main/java/com/example/prowd_android_template/activity_set/activity_init/ActivityInit.kt
@@ -3,12 +3,14 @@ package com.example.prowd_android_template.activity_set.activity_init
 import android.Manifest
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.prowd_android_template.activity_set.activity_home.ActivityHome
 import com.example.prowd_android_template.custom_view.DialogBinaryChoose
@@ -43,12 +45,7 @@ class ActivityInit : AppCompatActivity() {
     var permissionRequestCallbackMbr: (((MutableMap<String, Boolean>) -> Unit))? = null
 
     // 앱 사용 권한 모음
-    private val applicationPermissionArrayMbr = arrayOf(
-        Manifest.permission.CAMERA,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.READ_EXTERNAL_STORAGE
-    )
+    private lateinit var applicationPermissionArrayMbr: Array<String>
 
 
     // ---------------------------------------------------------------------------------------------
@@ -161,6 +158,11 @@ class ActivityInit : AppCompatActivity() {
                 permissionRequestCallbackMbr = null
             }
 
+        // Manifest.xml 에 설정된 모든 권한 배열
+        applicationPermissionArrayMbr = packageManager.getPackageInfo(
+            packageName,
+            PackageManager.GET_PERMISSIONS
+        ).requestedPermissions
     }
 
     // viewModel 저장용 데이터 초기화
@@ -423,7 +425,7 @@ class ActivityInit : AppCompatActivity() {
                         "\n1. 푸시 권한 :\n유용한 정보를 얻을 수 있습니다.\n" +
                         "\n2. 카메라 사용 권한 :\n사진 찍기 서비스에 사용됩니다.\n" +
                         "\n3. 위치 정보 접근 권한 :\n위치 기반 컨텐츠 제공에 사용됩니다.\n" +
-                        "\n4. 외부 저장소 접근 권한 :\n갤러리 등, 디바이스 내부 정보 사용에 사용됩니다.",
+                        "\netc..",
                 null,
                 onCheckBtnClicked = {
                     viewModelMbr.confirmDialogInfoLiveDataMbr.value =
