227493082929d7926c2d113f0697ef824928d91e
loner <prowdloner@petlogon.com>
2022-06-17 16:46:48 +0900

    핀치 줌 알고리즘 정리

--- app/src/main/java/com/example/prowd_android_template/activity_set/activity_basic_camera2_api_sample/ActivityBasicCamera2ApiSample.kt
+++ app/src/main/java/com/example/prowd_android_template/activity_set/activity_basic_camera2_api_sample/ActivityBasicCamera2ApiSample.kt
@@ -444,75 +444,66 @@ class ActivityBasicCamera2ApiSample : AppCompatActivity() {
     }
 
     // 초기 뷰 설정
+    var beforeFingerSpacingMbr: Float? = null
     private fun viewSetting() {
-        bindingMbr.root.setOnClickListener {
-            cameraObjMbr.setZoomFactor(5f, onZoomSettingComplete = {
-                Log.e("f", it.toString())
-            })
-        }
-
-
         // 카메라 핀치 줌
-//        bindingMbr.cameraPreviewAutoFitTexture.setOnTouchListener(object : View.OnTouchListener {
-//            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-//                when (event!!.action) {
-//                    MotionEvent.ACTION_DOWN -> {}
-//                    MotionEvent.ACTION_UP -> v!!.performClick()
-//                    else -> {}
-//                }
-//
-//                if (event.pointerCount == 2) { // 핀치를 위한 더블 터치일 경우
-//                    val currentFingerSpacing: Float
-//
-//                    val x = event.getX(0) - event.getX(1)
-//                    val y = event.getY(0) - event.getY(1)
-//
-//                    currentFingerSpacing = sqrt((x * x + y * y).toDouble()).toFloat()
-//                    var delta = 0.05f
-//                    if (viewModelMbr.beforeFingerSpacingMbr != 0f) {
-//                        if (currentFingerSpacing > viewModelMbr.beforeFingerSpacingMbr) { // 손가락을 벌린 경우
-//                            if (cameraObjMbr.maxZoomMbr - viewModelMbr.zoomLevelMbr <= delta) {
-//                                delta =
-//                                    cameraObjMbr.maxZoomMbr - viewModelMbr.zoomLevelMbr
-//                            }
-//                            viewModelMbr.zoomLevelMbr += delta
-//                        } else if (currentFingerSpacing < viewModelMbr.beforeFingerSpacingMbr) { // 손가락을 좁힌 경오
-//                            if (viewModelMbr.zoomLevelMbr - delta < 1f) {
-//                                delta = viewModelMbr.zoomLevelMbr - 1f
-//                            }
-//                            viewModelMbr.zoomLevelMbr -= delta
-//                        }
-//
-//                        cameraObjMbr.setCameraRequest(
-//                            onCameraRequestSettingTime = {
-//                                cameraObjMbr.setZoomRequest(
-//                                    it,
-//                                    viewModelMbr.zoomLevelMbr
-//                                )
-//                            },
-//                            onCameraRequestSetComplete = {
-//                                cameraObjMbr.runCameraRequest(
-//                                    true,
-//                                    null,
-//                                    onRequestComplete = {
-//
-//                                    },
-//                                    onError = {
-//
-//                                    })
-//                            },
-//                            onError = {
-//
-//                            })
-//                    }
-//                    viewModelMbr.beforeFingerSpacingMbr = currentFingerSpacing
-//
-//                    return true
-//                } else {
-//                    return true
-//                }
-//            }
-//        })
+        // todo : CameraObj 내장
+        bindingMbr.cameraPreviewAutoFitTexture.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                // 형식 맞추기 코드
+                when (event!!.action) {
+                    MotionEvent.ACTION_DOWN -> {}
+                    MotionEvent.ACTION_UP -> {
+                        v!!.performClick()
+
+                        // 손가락을 떼면 기존 핀치 너비 비우기
+                        beforeFingerSpacingMbr = null
+                    }
+                    else -> {}
+                }
+
+                if (event.pointerCount == 2) { // 핀치를 위한 더블 터치일 경우
+                    // 현재 핀치 넓이 구하기
+                    val currentFingerSpacing: Float
+                    val x = event.getX(0) - event.getX(1)
+                    val y = event.getY(0) - event.getY(1)
+                    currentFingerSpacing = sqrt((x * x + y * y).toDouble()).toFloat()
+
+                    val delta = 0.05f
+
+                    if (beforeFingerSpacingMbr != null) {
+                        if (currentFingerSpacing > beforeFingerSpacingMbr!!) { // 손가락을 벌린 경우
+                            val zoom =
+                                if ((cameraObjMbr.currentCameraZoomFactorMbr + delta) > cameraObjMbr.maxZoomMbr) {
+                                    cameraObjMbr.maxZoomMbr
+                                } else {
+                                    cameraObjMbr.currentCameraZoomFactorMbr + delta
+                                }
+                            cameraObjMbr.setZoomFactor(
+                                zoom,
+                                onZoomSettingComplete = {})
+                        } else if (currentFingerSpacing < beforeFingerSpacingMbr!!) { // 손가락을 좁힌 경우
+                            val zoom =
+                                if ((cameraObjMbr.currentCameraZoomFactorMbr - delta) < 1.0f) {
+                                    1.0f
+                                } else {
+                                    cameraObjMbr.currentCameraZoomFactorMbr - delta
+                                }
+                            cameraObjMbr.setZoomFactor(
+                                zoom,
+                                onZoomSettingComplete = {})
+                        }
+                    }
+
+                    // 핀치 너비를 갱신
+                    beforeFingerSpacingMbr = currentFingerSpacing
+
+                    return true
+                } else {
+                    return true
+                }
+            }
+        })
 
         val deviceOrientation: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             display!!.rotation
