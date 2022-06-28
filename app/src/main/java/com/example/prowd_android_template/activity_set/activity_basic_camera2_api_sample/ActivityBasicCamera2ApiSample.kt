1a438d5158b6dcba142ee5e56d89001725761d1a
loner <prowdloner@petlogon.com>
2022-06-28 12:50:33 +0900

    -

--- app/src/main/java/com/example/prowd_android_template/activity_set/activity_basic_camera2_api_sample/ActivityBasicCamera2ApiSample.kt
+++ app/src/main/java/com/example/prowd_android_template/activity_set/activity_basic_camera2_api_sample/ActivityBasicCamera2ApiSample.kt
@@ -1194,7 +1194,6 @@ class ActivityBasicCamera2ApiSample : AppCompatActivity() {
     }
 
     private fun onCameraPermissionChecked(isOnCreate: Boolean) {
-        Log.e("awb", cameraObjMbr.autoWhiteBalanceSupportedMbr.toString())
         if (isOnCreate) { // 처음 카메라 설정 시점
             // (카메라 실행)
             val previewConfigVo = if (null != cameraObjMbr.previewSurfaceSupportedSizeListMbr) {
