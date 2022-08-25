843713f79429d10de0b885bd06f29bb0d02f16a2
loner <prowdloner@petlogon.com>
2022-08-25 17:12:31 +0900

    -

--- app/src/main/java/com/example/prowd_android_template/activity_set/activity_basic_bottom_sheet_navigation_sample/fragment1/FragmentActivityBasicBottomSheetNavigationSampleFragment1.kt
+++ app/src/main/java/com/example/prowd_android_template/activity_set/activity_basic_bottom_sheet_navigation_sample/fragment1/FragmentActivityBasicBottomSheetNavigationSampleFragment1.kt
@@ -1,5 +1,8 @@
 package com.example.prowd_android_template.activity_set.activity_basic_bottom_sheet_navigation_sample.fragment1
 
+import android.content.res.Configuration
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
+import android.content.res.Configuration.ORIENTATION_PORTRAIT
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,10 +11,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.prowd_android_template.activity_set.activity_basic_bottom_sheet_navigation_sample.ActivityBasicBottomSheetNavigationSample
-import com.example.prowd_android_template.common_shared_preference_wrapper.CurrentLoginSessionInfoSpw
 import com.example.prowd_android_template.databinding.FragmentActivityBasicBottomSheetNavigationSampleFragment1Binding
 
-// todo : 신코드 적용
 // 플래그먼트 필수 권한은 이를 사용하는 액티비티에서 처리를 할 것 = 권한 충족이 되었다고 가정
 class FragmentActivityBasicBottomSheetNavigationSampleFragment1 : Fragment() {
     // <멤버 변수 공간>
@@ -25,14 +26,6 @@ class FragmentActivityBasicBottomSheetNavigationSampleFragment1 : Fragment() {
     val uiThreadHandlerMbr: Handler = Handler(Looper.getMainLooper())
 
     // (SharedPreference 객체)
-    // 현 로그인 정보 접근 객체
-    lateinit var currentLoginSessionInfoSpwMbr: CurrentLoginSessionInfoSpw
-
-    // (최초 실행 플래그) : 액티비티가 실행되고, 권한 체크가 끝난 후의 최초 로직이 실행되었는지 여부
-    var doItAlreadyMbr = false
-
-    // (이 화면에 도달한 유저 계정 고유값) : 세션 토큰이 없다면 비회원 상태
-    var currentUserSessionTokenMbr: String? = "currentUserSessionTokenMbr Not Init"
 
 
     // ---------------------------------------------------------------------------------------------
@@ -54,41 +47,52 @@ class FragmentActivityBasicBottomSheetNavigationSampleFragment1 : Fragment() {
         return bindingMbr.root
     }
 
+    private var doItAlreadyMbr = false
+    private var currentUserSessionTokenMbr: String? = null
     override fun onResume() {
         super.onResume()
 
         if (!doItAlreadyMbr) {
-            // (onCreate + permissionGrant)
             doItAlreadyMbr = true
 
             // (초기 데이터 수집)
+            currentUserSessionTokenMbr = parentActivityMbr.currentLoginSessionInfoSpwMbr.sessionToken
+            getScreenDataAndShow()
 
             // (알고리즘)
         } else {
             // (onResume - (onCreate + permissionGrant)) : 권한 클리어
 
-            // (알고리즘)
+            // (유저별 데이터 갱신)
+            // : 유저 정보가 갱신된 상태에서 다시 현 액티비티로 복귀하면 자동으로 데이터를 다시 갱신합니다.
+            val sessionToken = parentActivityMbr.currentLoginSessionInfoSpwMbr.sessionToken
+            if (sessionToken != currentUserSessionTokenMbr) { // 액티비티 유저와 세션 유저가 다를 때
+                // 진입 플래그 변경
+                currentUserSessionTokenMbr = sessionToken
+
+                // (데이터 수집)
+                getScreenDataAndShow()
+            }
         }
 
         // (onResume)
-        // (알고리즘)
-        // (뷰 데이터 로딩)
-        // : 데이터 갱신은 유저 정보가 변경된 것을 기준으로 함.
-        val sessionToken = currentLoginSessionInfoSpwMbr.sessionToken
-        if (sessionToken != currentUserSessionTokenMbr) { // 액티비티 유저와 세션 유저가 다를 때
-            // 진입 플래그 변경
-            currentUserSessionTokenMbr = sessionToken
-
-            // (데이터 수집)
-
-            // (알고리즘)
-        }
-
         if (parentActivityMbr.fragmentClickedPositionMbr == null){
             bindingMbr.clickedByValueTxt.text = "클릭 없음"
         }else{
             val textMsg = "${parentActivityMbr.fragmentClickedPositionMbr}번 플래그먼트"
             bindingMbr.clickedByValueTxt.text = textMsg
+        }
+    }
+
+    // (AndroidManifest.xml 에서 configChanges 에 설정된 요소에 변경 사항이 존재할 때 실행되는 콜백)
+    // : 해당 이벤트 발생시 처리할 로직을 작성
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (resources.configuration.orientation == ORIENTATION_LANDSCAPE) { // 화면회전 landscape
+
+        } else if (resources.configuration.orientation == ORIENTATION_PORTRAIT) { // 화면회전 portrait
+
         }
     }
 
@@ -103,8 +107,6 @@ class FragmentActivityBasicBottomSheetNavigationSampleFragment1 : Fragment() {
     private fun onCreateViewInitObject() {
         // (부모 객체 저장)
         parentActivityMbr = requireActivity() as ActivityBasicBottomSheetNavigationSample
-
-        currentLoginSessionInfoSpwMbr = CurrentLoginSessionInfoSpw(parentActivityMbr.application)
 
         // (뷰 바인딩)
         bindingMbr =
@@ -122,6 +124,12 @@ class FragmentActivityBasicBottomSheetNavigationSampleFragment1 : Fragment() {
                 bindingMbr.clickedByValueTxt.text = textMsg
             }
         }
+
+    }
+
+    // (화면 구성용 데이터를 가져오기)
+    // : 네트워크 등 레포지토리에서 데이터를 가져오고 이를 뷰에 반영
+    private fun getScreenDataAndShow() {
 
     }
 
