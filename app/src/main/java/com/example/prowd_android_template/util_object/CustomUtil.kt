55bd832eeed765b4ff4ac4523c84c46b6397bb24
DESKTOP-7POG9PR\Prowd <prowdloner@gmail.com>
2022-08-26 21:48:42 +0900

    -

--- app/src/main/java/com/example/prowd_android_template/util_object/CustomUtil.kt
+++ app/src/main/java/com/example/prowd_android_template/util_object/CustomUtil.kt
@@ -163,10 +163,4 @@ object CustomUtil {
     // 최소공배수
     fun getLcm(a: Int, b: Int): Int =
         (a * b) / getGcd(a, b)
-
-    // (현 화면상 dp 를 px 로 변환해주는 함수)
-    fun convertDPtoPX(context: Context, dp: Int): Int {
-        val density = context.resources.displayMetrics.density
-        return (dp.toFloat() * density).roundToInt()
-    }
 }
