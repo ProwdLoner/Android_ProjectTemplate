bef8e83e73ef753523956910cc3cc0ab82a345bb
DESKTOP-7POG9PR\Prowd <prowdloner@gmail.com>
2022-08-28 18:35:07 +0900

    -

--- app/src/main/java/com/example/prowd_android_template/activity_set/activity_dp_and_px_calculator/ActivityDpAndPxCalculator.kt
+++ app/src/main/java/com/example/prowd_android_template/activity_set/activity_dp_and_px_calculator/ActivityDpAndPxCalculator.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.prowd_android_template.R
 
 // todo : 신코드 적용
+// todo : 완성
 class ActivityDpAndPxCalculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/prowd_android_template/activity_set/activity_touch_delegate_sample/ActivityTouchDelegateSample.kt
+++ app/src/main/java/com/example/prowd_android_template/activity_set/activity_touch_delegate_sample/ActivityTouchDelegateSample.kt
@@ -26,6 +26,7 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Semaphore
 
+// todo : 완성
 class ActivityTouchDelegateSample : AppCompatActivity() {
     // <설정 변수 공간>
     // (앱 진입 필수 권한 배열)
