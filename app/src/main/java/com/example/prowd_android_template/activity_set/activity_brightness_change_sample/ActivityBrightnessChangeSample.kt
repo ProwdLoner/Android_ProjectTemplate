2346bc1f5cd1b21c5ca95156433828c5fe1dbdea
DESKTOP-7POG9PR\Prowd <prowdloner@gmail.com>
2022-08-28 20:07:16 +0900

    -

--- app/src/main/java/com/example/prowd_android_template/activity_set/activity_brightness_change_sample/ActivityBrightnessChangeSample.kt
+++ app/src/main/java/com/example/prowd_android_template/activity_set/activity_brightness_change_sample/ActivityBrightnessChangeSample.kt
@@ -13,6 +13,7 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.example.prowd_android_template.abstract_class.AbstractProwdRecyclerViewAdapter
 import com.example.prowd_android_template.abstract_class.InterfaceDialogInfoVO
 import com.example.prowd_android_template.common_shared_preference_wrapper.CurrentLoginSessionInfoSpw
 import com.example.prowd_android_template.custom_view.DialogBinaryChoose
@@ -21,11 +22,11 @@ import com.example.prowd_android_template.custom_view.DialogProgressLoading
 import com.example.prowd_android_template.custom_view.DialogRadioButtonChoose
 import com.example.prowd_android_template.databinding.ActivityBrightnessChangeSampleBinding
 import com.example.prowd_android_template.repository.RepositorySet
+import com.example.prowd_android_template.util_class.ThreadConfluenceObj
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Semaphore
 
-// todo : 신코드 적용
 // 액티비티 밝기 조절 샘플
 // 밝기 조절은 시스템 밝기 변화가 아닌 해당 액티비티에만 적용됨
 class ActivityBrightnessChangeSample : AppCompatActivity() {
@@ -43,6 +44,9 @@ class ActivityBrightnessChangeSample : AppCompatActivity() {
 
     // (repository 모델)
     lateinit var repositorySetMbr: RepositorySet
+
+    // (어뎁터 객체)
+    lateinit var adapterSetMbr: ActivityBrightnessChangeSampleAdapterSet
 
     // (SharedPreference 객체)
     // 현 로그인 정보 접근 객체
@@ -365,6 +369,10 @@ class ActivityBrightnessChangeSample : AppCompatActivity() {
         // 레포지토리 객체
         repositorySetMbr = RepositorySet.getInstance(application)
 
+        // 어뎁터 셋 객체 생성 (어뎁터 내부 데이터가 포함된 객체)
+        adapterSetMbr = ActivityBrightnessChangeSampleAdapterSet()
+
+        // SPW 객체 생성
         currentLoginSessionInfoSpwMbr = CurrentLoginSessionInfoSpw(application)
 
         // 권한 요청 객체 생성
@@ -429,7 +437,7 @@ class ActivityBrightnessChangeSample : AppCompatActivity() {
 
             // (초기 데이터 수집)
             currentUserUidMbr = currentLoginSessionInfoSpwMbr.userUid
-            getScreenDataAndShow()
+            refreshWholeScreenData(onComplete = {})
 
         } else {
             // (onResume - (권한이 충족된 onCreate))
@@ -442,7 +450,7 @@ class ActivityBrightnessChangeSample : AppCompatActivity() {
                 currentUserUidMbr = userUid
 
                 // (데이터 수집)
-                getScreenDataAndShow()
+                refreshWholeScreenData(onComplete = {})
             }
 
         }
@@ -450,10 +458,163 @@ class ActivityBrightnessChangeSample : AppCompatActivity() {
         // (onResume)
     }
 
+    // 화면 데이터 갱신관련 세마포어
+    private val screenDataSemaphoreMbr = Semaphore(1)
+
     // (화면 구성용 데이터를 가져오기)
     // : 네트워크 등 레포지토리에서 데이터를 가져오고 이를 뷰에 반영
-    private fun getScreenDataAndShow() {
+    //     onComplete = 네트워크 실패든 성공이든 데이터 요청 후 응답을 받아와 해당 상태에 따라 스크린 뷰 처리를 완료한 시점
+    //     'c숫자' 로 표기된 부분은 원하는대로 커스텀
+    private fun refreshWholeScreenData(onComplete: () -> Unit) {
+        executorServiceMbr.execute {
+            screenDataSemaphoreMbr.acquire()
 
+            runOnUiThread {
+                // (c1. 리스트 초기화)
+
+                // (c2. 로더 추가)
+            }
+
+            // (스레드 합류 객체 생성)
+            // : 헤더, 푸터, 아이템 리스트의 각 데이터를 비동기적으로 요청했을 때, 그 합류용으로 사용되는 객체
+            //     numberOfThreadsBeingJoinedMbr 에 비동기 처리 개수를 적고,
+            //     각 처리 완료시마다 threadComplete 를 호출하면 됨
+            val threadConfluenceObj =
+                ThreadConfluenceObj(
+                    3,
+                    onComplete = {
+                        screenDataSemaphoreMbr.release()
+                        onComplete()
+                    }
+                )
+
+            // (정보 요청 콜백)
+            // 아이템 리스트
+            // : statusCode
+            //     서버 반환 상태값. 1이라면 정상동작, -1 이라면 타임아웃, 2 이상 값들 중 서버에서 정한 상태값 처리, 그외엔 서버 에러
+            //     1 이외의 상태값에서 itemList 는 null
+            val getItemListOnComplete: (statusCode: Int, itemList: ArrayList<AbstractProwdRecyclerViewAdapter.AdapterItemAbstractVO>?) -> Unit =
+                { statusCode, itemList ->
+                    runOnUiThread {
+                        // (c3. 로더 제거)
+                    }
+
+                    when (statusCode) {
+                        1 -> { // 완료
+                            if (itemList!!.isEmpty()) { // 받아온 리스트가 비어있을 때
+                                // (c4. 빈 리스트 처리)
+
+                                threadConfluenceObj.threadComplete()
+                            } else {
+                                runOnUiThread {
+                                    // (c5. 받아온 아이템 추가)
+
+                                    // (c6. 스크롤을 가장 앞으로 이동)
+                                }
+
+                                threadConfluenceObj.threadComplete()
+                            }
+                        }
+                        -1 -> { // 네트워크 에러
+                            // (c7. 네트워크 에러 처리)
+
+                            threadConfluenceObj.threadComplete()
+                        }
+                        else -> { // 그외 서버 에러그외 서버 에러
+                            // (c8. 그외 서버 에러 처리)
+
+                            threadConfluenceObj.threadComplete()
+                        }
+                    }
+                }
+
+            // 헤더 아이템
+            // : statusCode
+            //     서버 반환 상태값. 1이라면 정상동작, -1 이라면 타임아웃, 2 이상 값들 중 서버에서 정한 상태값 처리, 그외엔 서버 에러
+            //     1 이외의 상태값에서 item 은 null
+            val getHeaderItemOnComplete: (statusCode: Int, item: AbstractProwdRecyclerViewAdapter.AdapterHeaderAbstractVO?) -> Unit =
+                { statusCode, item ->
+                    runOnUiThread {
+                        // (c9. 로더 제거)
+                    }
+
+                    when (statusCode) {
+                        1 -> { // 완료
+                            runOnUiThread {
+                                // (c10. 받아온 아이템 추가)
+                            }
+
+                            threadConfluenceObj.threadComplete()
+                        }
+                        -1 -> { // 네트워크 에러
+                            // (c11. 네트워크 에러 처리)
+
+                            threadConfluenceObj.threadComplete()
+                        }
+                        else -> { // 그외 서버 에러
+                            // (c12. 그외 서버 에러 처리)
+
+                            threadConfluenceObj.threadComplete()
+                        }
+                    }
+                }
+
+            // 푸터 아이템
+            // : statusCode
+            //     서버 반환 상태값. 1이라면 정상동작, -1 이라면 타임아웃, 2 이상 값들 중 서버에서 정한 상태값 처리, 그외엔 서버 에러
+            //     1 이외의 상태값에서 item 은 null
+            val getFooterItemOnComplete: (statusCode: Int, item: AbstractProwdRecyclerViewAdapter.AdapterFooterAbstractVO?) -> Unit =
+                { statusCode, item ->
+                    runOnUiThread {
+                        // (c13. 로더 제거)
+                    }
+
+                    when (statusCode) {
+                        1 -> {// 완료
+                            runOnUiThread {
+                                // (c14. 받아온 아이템 추가)
+                            }
+
+                            threadConfluenceObj.threadComplete()
+                        }
+                        -1 -> { // 네트워크 에러
+                            // (c15. 네트워크 에러 처리)
+
+                            threadConfluenceObj.threadComplete()
+                        }
+                        else -> { // 그외 서버 에러
+                            // (c16. 그외 서버 에러 처리)
+
+                            threadConfluenceObj.threadComplete()
+                        }
+                    }
+                }
+
+            // (네트워크 요청)
+            // (c17. 아이템 리스트 가져오기)
+            // : lastItemUid 등의 인자값을 네트워크 요청으로 넣어주고 데이터를 받아와서 onComplete 실행
+            //     데이터 요청 API 는 정렬기준, 마지막 uid, 요청 아이템 개수 등을 입력하여 데이터 리스트를 반환받음
+            executorServiceMbr.execute {
+                getItemListOnComplete(-2, null)
+            }
+
+            // (c18. 헤더 데이터 가져오기)
+            // : lastItemUid 등의 인자값을 네트워크 요청으로 넣어주고 데이터를 받아와서 onComplete 실행
+            //     데이터 요청 API 는 정렬기준, 마지막 uid, 요청 아이템 개수 등을 입력하여 데이터 리스트를 반환받음
+            executorServiceMbr.execute {
+                getHeaderItemOnComplete(-2, null)
+            }
+
+            // (c19. 푸터 데이터 가져오기)
+            // : lastItemUid 등의 인자값을 네트워크 요청으로 넣어주고 데이터를 받아와서 onComplete 실행
+            //     데이터 요청 API 는 정렬기준, 마지막 uid, 요청 아이템 개수 등을 입력하여 데이터 리스트를 반환받음
+            executorServiceMbr.execute {
+                getFooterItemOnComplete(-2, null)
+            }
+
+            // (c20. 그외 스크린 데이터 가져오기)
+
+        }
     }
 
 
