0768b36e151a909d54521064b60973c46a75d40a
DESKTOP-7POG9PR\Prowd <prowdloner@gmail.com>
2022-09-14 09:24:47 +0900

    -

--- app/src/main/java/com/example/prowd_android_template/util_class/LogObj.kt
+++ app/src/main/java/com/example/prowd_android_template/util_class/LogObj.kt
@@ -2,6 +2,7 @@ package com.example.prowd_android_template.util_class
 
 import android.content.Context
 import android.util.Log
+import com.example.prowd_android_template.BuildConfig
 import com.example.prowd_android_template.util_object.CustomUtil
 
 // [잘림 방지용 커스텀 로그 객체]
@@ -46,7 +47,7 @@ class LogObj constructor(val logName: String) {
 
     // (디버그 모드에만 사용되는 로그)
     fun di(context: Context, message: String) {
-        if (!CustomUtil.isDebuggable(context)) {
+        if (!BuildConfig.DEBUG) {
             return
         }
         if (message.length > 3000) {    // 텍스트가 3000자 이상이 넘어가면 줄
@@ -58,7 +59,7 @@ class LogObj constructor(val logName: String) {
     }
 
     fun dv(context: Context, message: String) {
-        if (!CustomUtil.isDebuggable(context)) {
+        if (!BuildConfig.DEBUG) {
             return
         }
         if (message.length > 3000) {    // 텍스트가 3000자 이상이 넘어가면 줄
@@ -70,7 +71,7 @@ class LogObj constructor(val logName: String) {
     }
 
     fun dw(context: Context, message: String) {
-        if (!CustomUtil.isDebuggable(context)) {
+        if (!BuildConfig.DEBUG) {
             return
         }
         if (message.length > 3000) {    // 텍스트가 3000자 이상이 넘어가면 줄
@@ -82,7 +83,7 @@ class LogObj constructor(val logName: String) {
     }
 
     fun de(context: Context, message: String) {
-        if (!CustomUtil.isDebuggable(context)) {
+        if (!BuildConfig.DEBUG) {
             return
         }
         if (message.length > 3000) {    // 텍스트가 3000자 이상이 넘어가면 줄
